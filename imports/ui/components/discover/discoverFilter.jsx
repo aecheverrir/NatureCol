7c25ad29cd6b931d8768ef2b30371b3efd8288de
s-cortes <s.cortes@uniandes.edu.co>
2018-05-18 16:06:44 -0500

    filtro inicial para observaciones, metodos para obtener especies y consultar observaciones

--- imports/ui/components/discover/discoverFilter.jsx
+++ imports/ui/components/discover/discoverFilter.jsx
@@ -1,23 +1,62 @@
 import React, { Component } from "react";
-import {Row, Col } from 'react-bootstrap';
+import { Row, Col, Form, FormGroup, ControlLabel, FormControl, Button, Glyphicon } from 'react-bootstrap';
 
-export default class DiscoverContainer extends Component {
+export default class DiscoverFilter extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      commonName: ''
     }
+    this.handleSpeciesSubmit = this.handleSpeciesSubmit.bind(this);
+    this.handleCommonNameChange = this.handleCommonNameChange.bind(this);
+  }
+
+  handleSpeciesSubmit(event) {
+    event.preventDefault();
+    if(this.state.commonName.length > 0){
+      //Does something
+      console.log('Query Param:', this.state.commonName);
+    }
+    else{
+      //Does something else
+    }
+  }
+
+  handleCommonNameChange(event){
+    console.log(event.target.value);
+    this.setState({
+      commonName: event.target.value
+    });
   }
 
   render() {
     return (
-      <div>
-        <Row>
-          <Col md={12}>
-            <h1>Discover Container</h1>
-          </Col>
-        </Row>
-      </div>
+      <Row>
+        <Col className='' md={12}>
+          <Row>
+            <Col md={4}>
+              <h1>Discover </h1>
+            </Col>
+            <Col md={6}>
+              <Form inline onSubmit={this.handleSpeciesSubmit}
+                style={{marginTop: '10px'}}>
+                <FormGroup controlId="formInlineName">
+                  <ControlLabel>
+                    Search Species
+                  </ControlLabel>{'   '}
+                  <FormControl type="text" 
+                    placeholder="e.g. Bear" 
+                    onChange={this.handleCommonNameChange}/>
+                </FormGroup>{' '}
+                <Button type="submit">Send invitation</Button>
+              </Form>
+            </Col>
+            <Col md={2}>
+              
+            </Col>
+          </Row>
+        </Col>
+      </Row>
     );
   }
 }
--- imports/ui/pages/discover/discoverContainer.jsx
+++ imports/ui/pages/discover/discoverContainer.jsx
@@ -1,22 +1,48 @@
 import React, { Component } from "react";
 import { Grid, Row, Col } from 'react-bootstrap';
 
+import DiscoverFilter from "../../components/discover/discoverFilter";
+
 export default class DiscoverContainer extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-
+			speciesList: [],
+			observations: []
 		}
+		this.setSpeciesList = this.setSpeciesList.bind(this);
+	}
+
+	setSpeciesList(commonName) {
+		Meteor.call('iNaturalist.getTaxonIds', commonName, (err, res) => {
+			if(err){
+				console.err(err);
+			}
+			else{
+				this.setState({
+					speciesList: []
+				});
+			}
+		});
+	}
+
+	getObservations(queryParams, pageNum, itemsPerPage) {
+		Meteor.call('iNaturalist.getObservations', queryParams, pageNum, itemsPerPage, (err, res) => {
+			if (err) {
+				console.err(err);
+			}
+			else {
+				this.setState({
+					observations: []
+				});
+			}
+		});
 	}
 
 	render() {
 		return(
 			<div>
-				<Row>
-					<Col md={12}>
-						<h1>Discover Container</h1>
-					</Col>
-				</Row>
+				<DiscoverFilter />
 			</div>
 		);
 	}
