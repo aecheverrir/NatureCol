e8dc30dd67c365180a91d2245fb5eef324d86162
s-cortes <s.cortes@uniandes.edu.co>
2018-05-19 06:58:06 -0500

    pagination

--- imports/ui/components/discover/discoverList.jsx
+++ imports/ui/components/discover/discoverList.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Grid, Row, Col, Button, Table } from "react-bootstrap";
+import { Grid, Row, Col, ButtonGroup, Button, Table } from "react-bootstrap";
 import { Meteor } from "meteor/meteor";
 import { withRouter } from "react-router-dom";
 import { withTracker } from "meteor/react-meteor-data";
@@ -15,15 +15,7 @@ class DiscoverList extends Component {
 
 
   componentDidMount() {
-    this.props.getObservations({ }, this.state.pageNumber, this.state.itemsPerPage);
-  }
-
-  componentWillUpdate(props){
-    if(props.newSearch){
-      this.setState({
-        pageNumber: 1
-      });
-    }
+    this.props.getObservations(null, this.state.pageNumber);
   }
 
   renderItems() {
@@ -35,10 +27,36 @@ class DiscoverList extends Component {
     });
   }
 
+  previous() {
+    let newPage = this.state.pageNumber - 1;
+    this.props.getObservations(null, newPage);
+    this.setState({
+      pageNumber: newPage
+    });
+  }
+
+  next() {
+    let newPage = this.state.pageNumber + 1;
+    this.props.getObservations(null, newPage);
+    this.setState({
+      pageNumber: newPage
+    });
+  }
+
   render() {
     return (
       <Grid>
         <Row>
+          <Col md={12}>
+            <Row>
+              <Col md={12}>
+                <ButtonGroup>
+                  <Button bsStyle="info" disabled={this.state.pageNumber === 1} onClick={this.previous.bind(this)}>Previous</Button>
+                  <Button bsStyle="info" disabled={this.state.pageNumber === this.props.maxPage} onClick={this.next.bind(this)}>Next</Button>
+                </ButtonGroup>
+              </Col>
+            </Row>
+          </Col>
           <Col md={12}>
             <Row>
               {this.renderItems()}
